78e122b48b8dca3f03264abaafc095c54323ffcd
adday <adday@email.arizona.edu>
2019-10-28 13:51:02 -0400

    Cleaned up the comments

--- collatz.py
+++ collatz.py
@@ -1,5 +1,4 @@
 from p5 import *
-from random import randrange
 from typing import Tuple
 
 
@@ -45,86 +44,123 @@ def collatz(n: int) -> int:
 
 
 def color(step: int=10) -> Tuple[int, int, int]:
+    """Generator for the color sequence. It is randomly "seeded" and
+    then each rgb goes up or down randomly
 
-    r, g, b = randrange(0, 255), randrange(0, 255), randrange(0, 255)
-    r_inc = bool(randrange(0, 1))
-    g_inc = bool(randrange(0, 1))
-    b_inc = bool(randrange(0, 1))
+    Args:
+        step (int, optional): How big the step is between each generation
+
+    Yields:
+        Tuple[int, int, int]: red, green, blue values
+    """
+    # Randomly seed the r g b values
+    r, g, b = (random_uniform(0, 255), random_uniform(0, 255),
+               random_uniform(0, 255))
+
+    # Randomly determine if each r g and b value is increasing or not
+    r_inc = True
+    g_inc = True
+    b_inc = True
+    r_step = random_uniform(step)
+    g_step = random_uniform(step)
+    b_step = random_uniform(step)
+
+    # Yield the initial r, g, b values
     yield r, g, b
+
+    # Loop and yeild forever
     while True:
+        # If r is increasing
         if r_inc:
-            r += step
-            r_inc = r < 255 - step
+            # Increment r by the step
+            r += r_step
+            # Ensure that the next step will be within the limits
+            # if not then set the flag to decreasing
+            r_inc = r < 255 - r_step
+        # If r is decreasing
         else:
-            r -= step
-            r_inc = r < step
+            # Decrement r by the step
+            r -= r_step
+            # Ensure that the next step will be within the limits
+            # if not then set the flag to increasing
+            r_inc = r < r_step
 
+        # See above
         if g_inc:
-            g += step
-            g_inc = g < 255 - step
+            g += g_step
+            g_inc = g < 255 - g_step
         else:
-            g -= step
-            g_inc = g < step
+            g -= g_step
+            g_inc = g < g_step
 
+        # See above
         if b_inc:
-            b += step
-            r_inc = b < 255 - step
+            b += b_step
+            b_inc = b < 255 - b_step
         else:
-            b -= step
-            r_inc = b < step
+            b -= b_step
+            b_inc = b < b_step
 
+        # Yield the red, green, and blue values
         yield r, g, b
 
 
 def setup():
+    """Set up the canvas as a 800, 600 window on the screen
+    """
     size(800, 600)
-    background(51)
-
-
-i = 10
-colors = color()
+    stroke_weight(3)
 
 
 def draw():
-    global i
+    """Draw the new frame each loop of the program
+    """
 
-    if i == 10:
-        input(">")
+    max_frames = 1_000
+
+    # Normally this would be just in the setup but for some reason
+    # python P5 doesn't like just setting the background in the setup
+    # loop
+    if frame_count == 1:
         background(51)
 
-    colors = color()
-    r, g, b = next(colors)
+    # Instantiate a new generator for the colors of the line. The generator
+    # picks a random starting point and then
+    colors = color(5)
 
-    sequence = list(collatz(i))
-    # print(sequence)
+    # Set up the default length and angle
+    length = height / 35
+    angle = (width / height) * 0.25
 
-    length = 12
-    angle = 0.25
+    # Reset the matrix to the initial translation
     reset_matrix()
     translate(width / 2, height)
 
-    for collatz_number in reversed(sequence):
+    # Loop through the collatz sequence for the current frame count.
+    # Reversed so we can start at 1 and then go to n
+    for collatz_number in reversed(list(collatz(frame_count))):
+        # If the current number is even rotate to the right
         if collatz_number % 2 == 0:
             rotate(angle)
+
+        # If it is odd then rotate to the left with some extra "umph"
+        # because it looks better
         else:
             rotate(-angle * 1.5)
 
-        stroke_weight(5)
-
-        stroke(r, g, b, 10)
+        # Set the stroke to the current color and add some alpha so that
+        # overlapping paths have more color
         r, g, b = next(colors)
+        stroke(r, g, b, 20)
 
+        # Draw the line for the collatz sequence
         line((0, 0), (0, -length))
 
+        # Translate to the end of the line
         translate(0, -length)
 
-        # print("DONE")
-
-    i += 1
-
-    # print(i)
-
-    if i > 10_000:
+    # Check if we are at the end, if so stop looping
+    if frame_count > max_frames:
         no_loop()
 
 
